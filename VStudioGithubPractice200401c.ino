9149c706f555eb374c692031bb4ed661997c356c
Juan-Estrella <theenlightenedjuan@gmail.com>
2020-04-01 17:11:11 -0400

    Add project files.

---
+++ VStudioGithubPractice200401c.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		VStudioGithubPractice200401c.ino
+ Created:	01-Apr-20 5:10:31 PM
+ Author:	Juan Estrella
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
